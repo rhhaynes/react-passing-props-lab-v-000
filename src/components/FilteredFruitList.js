a786c5d9a7d2b7aefbb07c1378534b144139e72d
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 19:10:29 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FilteredFruitList = ({filter, fruit}) => {
+const FilteredFruitList = ({ filter, fruit }) => {
   const list = ( !filter || filter === 'all' ? fruit : fruit.filter(i => i.fruit_type === filter) );
   return (
     <ul className="fruit-list">
